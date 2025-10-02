2ac1682ece4b6bfc27e7241a60fda2d5c9a7f968
g11tech <gajinder@zeam.in>
2025-10-02 23:20:18 +0530

    delay genesis by 30 secs (#5)

--- set-up.sh
+++ set-up.sh
@@ -17,7 +17,7 @@ done;
 if [ -n "$freshStart" ]
 then
   TIME_NOW="$(date +%s)"
-  GENESIS_TIME=$((TIME_NOW))
+  GENESIS_TIME=$((TIME_NOW + 30))
   sedPatten="/GENESIS_TIME/c\GENESIS_TIME: $GENESIS_TIME"
   cmd="sed -i \"$sedPatten\" \"$configDir/config.yaml\""
   echo $cmd
