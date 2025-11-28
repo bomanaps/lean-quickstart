0c0c429477454e19a604d29f31589670a551dea4
kamilsa <kamilsa16@gmail.com>
2025-11-28 11:47:24 +0500

    Update qlean (#57)

--- client-cmds/qlean-cmd.sh
+++ client-cmds/qlean-cmd.sh
@@ -7,15 +7,21 @@ node_binary="$scriptDir/qlean/build/src/executable/qlean \
       --modules-dir $scriptDir/qlean/build/src/modules \
       --genesis $configDir/config.yaml \
       --validator-registry-path $configDir/validators.yaml \
+      --validator-keys-manifest $configDir/hash-sig-keys/validator-keys-manifest.yaml \
+      --xmss-pk $hashSigPkPath \
+      --xmss-sk $hashSigSkPath \
       --bootnodes $configDir/nodes.yaml \
       --data-dir $dataDir/$item \
       --node-id $item --node-key $configDir/$privKeyPath \
       --listen-addr /ip4/0.0.0.0/udp/$quicPort/quic-v1 \
       --prometheus-port $metricsPort"
       
-node_docker="--platform linux/amd64 qdrvm/qlean-mini:dd67521 \
+node_docker="qdrvm/qlean-mini:3fb5723 \
       --genesis /config/config.yaml \
       --validator-registry-path /config/validators.yaml \
+      --validator-keys-manifest /config/hash-sig-keys/validator-keys-manifest.yaml \
+      --xmss-pk /config/hash-sig-keys/validator_${hashSigKeyIndex}_pk.json \
+      --xmss-sk /config/hash-sig-keys/validator_${hashSigKeyIndex}_sk.json \
       --bootnodes /config/nodes.yaml \
       --data-dir /data \
       --node-id $item --node-key /config/$privKeyPath \
