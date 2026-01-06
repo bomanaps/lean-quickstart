aa0410bc1bbadb2bd814c0e0419723e9849d52ed
chethack/Chetany <95150398+chetanyb@users.noreply.github.com>
2026-01-06 23:56:16 +0530

    fix: remove ream devnet flag (#78)

--- client-cmds/ream-cmd.sh
+++ client-cmds/ream-cmd.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 #-----------------------ream setup----------------------
-devnet_flag=""
-if [ -n "$devnet" ]; then
-        devnet_flag="--devnet $devnet"
-fi
-
 # Metrics enabled by default
 metrics_flag="--metrics"
 
@@ -14,7 +9,6 @@ node_binary="$scriptDir/../ream/target/release/ream --data-dir $dataDir/$item \
         lean_node \
         --network $configDir/config.yaml \
         --validator-registry-path $configDir/validators.yaml \
-        $devnet_flag \
         --bootnodes $configDir/nodes.yaml \
         --node-id $item --node-key $configDir/$privKeyPath \
         --socket-port $quicPort \
@@ -27,7 +21,6 @@ node_docker="ghcr.io/reamlabs/ream:latest --data-dir /data \
         lean_node \
         --network /config/config.yaml \
         --validator-registry-path /config/validators.yaml \
-        $devnet_flag \
         --bootnodes /config/nodes.yaml \
         --node-id $item --node-key /config/$privKeyPath \
         --socket-port $quicPort \
